522cc2b9d637430fa43e95cf11b61b2c4c053eda
fiftinmen <fiftin.men@gmail.com>
2024-05-16 15:53:11 +0300

    fix checking if the key is added or removed

--- gendiff/diff_tools.py
+++ gendiff/diff_tools.py
@@ -2,36 +2,35 @@ def determine_type(value):
     return 'complex' if isinstance(value, dict) else 'simple'
 
 
-def generate_node(key, value1, value2):
-    type1 = determine_type(value1)
-    type2 = determine_type(value2)
-
-    status = 'unchanged'
-    values = {'values': value1}
-    types = {'type': type1}
-    if type1 == type2 == 'complex' and value1 != value2:
-        status = 'nested'
-        values = {'children': build_diff(value1, value2)}
-        types = {'type': 'diff'}
-    elif value1 is None:
-        status = 'added'
-        values['values'] = value2
-        types = {'type': type2}
-    elif value2 is None:
-        status = 'removed'
-    elif value1 != value2:
-        status = 'updated'
-        values = {'old_value': value1, 'new_value': value2}
-        types = {'old_type': type1,
-                 'new_type': type2}
-    return {'key': key, 'status': status} | values | types
-
-
 def build_diff(dict1, dict2):
     diff = []
-    for key in sorted(set(dict1) | set(dict2)):
+    keys1 = set(dict1)
+    keys2 = set(dict2)
+    keys = sorted(keys1 | keys2)
+    for key in keys:
         value1, value2 = dict1.get(key), dict2.get(key)
-        diff.append(generate_node(key, value1, value2))
+        type1 = determine_type(value1)
+        type2 = determine_type(value2)
+
+        status = 'unchanged'
+        values = {'values': value1}
+        types = {'type': type1}
+        if type1 == type2 == 'complex' and value1 != value2:
+            status = 'nested'
+            values = {'children': build_diff(value1, value2)}
+            types = {'type': 'diff'}
+        elif key not in keys1:
+            status = 'added'
+            values['values'] = value2
+            types = {'type': type2}
+        elif key not in keys2:
+            status = 'removed'
+        elif value1 != value2:
+            status = 'updated'
+            values = {'old_value': value1, 'new_value': value2}
+            types = {'old_type': type1,
+                     'new_type': type2}
+        diff.append({'key': key, 'status': status} | values | types)
     return diff
 
 
